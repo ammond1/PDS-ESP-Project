beb66cdf23c15786f3a654e121ad94dee1e4affd
ammond <33509574+ammond1@users.noreply.github.com>
2025-03-13 13:02:03 +0800

    bot implemented

---
+++ bot.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+import os
+import telebot
+import firebase_admin
+from firebase_admin import credentials, db
+import time
+
+# Path to your Firebase Admin SDK private key file
+cred = credentials.Certificate('/Users/admin/Desktop/pds-esp-project-firebase-adminsdk-fbsvc-78eb11400f.json')
+
+# Initialize Firebase Admin SDK
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://pds-esp-project-default-rtdb.firebaseio.com/'  # Replace with your Firebase Realtime Database URL
+})
+
+load_dotenv()
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+ref = db.reference('noise_level')
+listening = False
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    chatid = message.chat.id
+    bot.send_message(chat_id=chatid, text='Listening...')
+    global listening
+    listening = True
+    listen(chatid)
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    chatid = message.chat.id
+    bot.send_message(chat_id=chatid, text='Stopping...')
+    global listening
+    listening = False
+
+def listen(chatid):
+    global listening
+    while listening:
+        ref = db.reference('noise_level')
+        noise_level = ref.get()
+
+        if noise_level >200:
+            bot.send_message(chat_id=chatid, text= 'NOISY SIA')
+        time.sleep(5)
+
+bot.infinity_polling()
